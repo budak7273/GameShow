3521db89b2b4bcc3fac1b2982a00f156ba431189
Chris Wall <porisius@gmail.com>
2025-02-15 00:00:46 -0500

    Add HTTP module

--- Source/GameShow/GameShow.Build.cs
+++ Source/GameShow/GameShow.Build.cs
@@ -13,7 +13,8 @@ public class GameShow : ModuleRules
 		// Not all of these are required, but including the extra ones saves you from having to add them later.
 		// Some entries are commented out to avoid compile-time warnings about depending on a module that you don't explicitly depend on.
 		// You can uncomment these as necessary when your code actually needs to use them.
-		PublicDependencyModuleNames.AddRange(new string[] {
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
 			"Core", "CoreUObject",
 			"Engine",
 			"DeveloperSettings",
@@ -41,7 +42,8 @@ public class GameShow : ModuleRules
 			//"TemplateSequence",
 			"NetCore",
 			"GameplayTags",
-			"Json", "JsonUtilities"
+			"Json", "JsonUtilities",
+			"HTTP"
 		});
 
 		// FactoryGame plugins
