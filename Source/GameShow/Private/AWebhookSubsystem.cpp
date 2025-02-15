1f6154d90c78ec84271806b51809d5fe64a138ed
Chris Wall <porisius@gmail.com>
2025-02-15 18:26:08 -0500

    Removed unneed code, fixed payload launcher for Webhook, bump version

--- Source/GameShow/Private/AWebhookSubsystem.cpp
+++ Source/GameShow/Private/AWebhookSubsystem.cpp
@@ -9,25 +9,27 @@
 #include "Config/WebhookStruct.h"
 
 void AWebhookSubsystem::SendJsonToWebhook(FString JsonString) {
-	TSharedPtr<FJsonObject> JsonObject = MakeShareable(new FJsonObject);
 	FWebhookStruct config = FWebhookStruct::GetActiveConfig(this);
-
-	TSharedRef<TJsonWriter<>> Writer = TJsonWriterFactory<>::Create(&JsonString);
-	FJsonSerializer::Serialize(JsonObject.ToSharedRef(), Writer);
-
-	TSharedRef<IHttpRequest> HttpRequest = FHttpModule::Get().CreateRequest();
+				
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
 	HttpRequest->SetVerb("POST");
 	HttpRequest->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
 	HttpRequest->SetURL(config.WebhookURL);
 	HttpRequest->SetContentAsString(JsonString);
 
-	HttpRequest->OnProcessRequestComplete().BindLambda([](FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful) {
-		if (bWasSuccessful) {
-			UE_LOG(LogGameShow, Log, TEXT("Webhook response: %s"), *Response->GetContentAsString());
-		} else {
-			UE_LOG(LogGameShow, Error, TEXT("Failed to send data to webhook"));
+	//UE_LOGFMT(LogGameShow, Log, "JSON Payload: {JsonString}", JsonString);
+
+	HttpRequest->OnProcessRequestComplete().BindLambda([](FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
+	{
+		if (bWasSuccessful)
+		{
+			UE_LOG(LogTemp, Log, TEXT("Webhook response: %s"), *Response->GetContentAsString());
 		}
-		});
+		else
+		{
+			UE_LOG(LogTemp, Error, TEXT("Failed to send data to webhook"));
+		}
+	});
 
 	HttpRequest->ProcessRequest();
 };
